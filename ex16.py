a5097ca5d6f50f0cc369e6450ef94b99b307694b
Mattias Soderberg <mattias.soderberg@emc.com>
2017-09-21 15:25:34 +0200

    Update ex16.py

--- ex16.py
+++ ex16.py
@@ -4,7 +4,7 @@ from sys import argv
 
 script, filename = argv
 
-print "We're going to erase %r" % filename
+print "We're going to erase %r." % filename
 print "If you don't want that, hit CTRL-C."
 print "If you do want that, hit RETURN."
 
@@ -12,7 +12,6 @@ raw_input("?")
 
 print "Opening the file..."
 target = open(filename, 'w')
-raw_input()
 
 #print "Truncating the file! Goodbye!"
 #target.truncate()
@@ -24,6 +23,8 @@ line2 = raw_input("Line2: ")
 line3 = raw_input("Line3: ")
 
 lines_of_text = "%s\n%s\n%s\n" % (line1, line2, line3)
+print lines_of_text
 target.write(lines_of_text)
 print "And finally we close it!!"
 target.close()
+
